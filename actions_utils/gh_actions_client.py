7795ca9a8af28e758ee5843a07af5945f325d631
Eugene Fedorenko <eugene.fedor@gmail.com>
2020-04-27 15:21:22 -0700

    Dispatch (#8)

---
+++ actions_utils/gh_actions_client.py
@@ -0,0 +1,40 @@
+import os
+import requests
+
+class GhActionsClient:
+
+    def __init__(self, repo, pat):
+        self.owner, self.repo = repo.split("/")
+        self.personal_access_token = pat
+
+        self.base_url = f'https://api.github.com/repos/{self.owner}/{self.repo}'
+        self.headers = {'authorization': f'token {self.personal_access_token}',
+                   'accept': 'application/vnd.github.everest-preview+json'}
+
+    def send_dispatch_event(self, sha, pr_num, phase):
+        url = self.base_url + "/dispatches"
+        payload = {'sha': sha, 'pr_num': pr_num}
+        data = {'event_type': phase, 'client_payload': payload}
+        response = requests.post(url=url, headers=self.headers, json=data)
+        assert response.status_code == 204
+        print(response)
+
+    def add_comment(self, pr_num, comment):
+        url = self.base_url + "/issues/{pr_num}/comments".format(pr_num=pr_num)        
+        data = {'body': comment}
+        response = requests.post(url=url, headers=self.headers, json=data)
+        assert response.status_code == 201
+        print(response)
+        
+def get_gh_actions_client():
+    return GhActionsClient(os.getenv("GITHUB_TOKEN"), os.getenv("GITHUB_REPOSITORY"))
+
+if __name__ == "__main__":
+    pat = "27b2fa280166f4dd90c1f587671f0fba1f217d2d"
+    repo = "kaizentm/kubemlops"
+    client = GhActionsClient(repo, pat)
+    #client.send_dispatch_event(sha="", pr_num="", phase="training")
+    client.add_comment(pr_num="6", comment="Hello from Client")
+
+
+
