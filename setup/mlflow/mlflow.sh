618c6ad5f12d44a3c2946267962cacc77a3ccb08
David Tesar <david.tesar@microsoft.com>
2020-04-29 11:12:33 -0700

    Add mlflow install (#15)

---
+++ setup/mlflow/mlflow.sh
@@ -0,0 +1,7 @@
+# https://hub.kubeapps.com/charts/larribas/mlflow
+helm repo add larribas https://larribas.me/helm-charts
+helm install mlflow larribas/mlflow --set image.repository="dtzar/mlflow",image.tag="latest"
+
+# Access the UI
+k port-forward svc/mlflow 5000:5000
+http://localhost:5000
