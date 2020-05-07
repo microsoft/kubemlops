28f3a12130002096950c7ca70b3d2e0a1a458e57
Tom Care <tcare@microsoft.com>
2020-05-07 12:31:46 -0700

    Tell MLFlow server to expose prometheus style metrics (#30)

--- setup/mlflow/mlflow.sh
+++ setup/mlflow/mlflow.sh
@@ -1,6 +1,6 @@
 # https://hub.kubeapps.com/charts/larribas/mlflow
 helm repo add larribas https://larribas.me/helm-charts
-helm install mlflow larribas/mlflow --set image.repository="dtzar/mlflow",image.tag="latest"
+helm install mlflow larribas/mlflow --set image.repository="dtzar/mlflow",image.tag="latest",extraArgs.expose-prometheus="yes"
 
 # Access the UI
 k port-forward svc/mlflow 5000:5000
