6ed6c3a88b97f0d8ad99cf762057fd558f7c9f61
Michael Tarng <20913254+mtarng@users.noreply.github.com>
2020-05-27 01:44:19 -0700

    basic foundational terraform template for AKS cluster + ACR

---
+++ terraform/kubeflow/main.tf
@@ -0,0 +1,66 @@
+module "provider" {
+  source = "../../azure/provider"
+}
+
+data "azurerm_resource_group" "resource_group" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                     = var.acr_name
+  resource_group_name      = data.azurerm_resource_group.resource_group.name
+  location                 = data.azurerm_resource_group.resource_group.location
+  sku                      = var.acr_sku
+  admin_enabled            = var.acr_admin_enabled
+  georeplication_locations = var.acr_georeplication_locations
+
+module "vnet" {
+  source = "../../azure/vnet"
+
+  resource_group_name     = data.azurerm_resource_group.resource_group.name
+  vnet_name               = var.vnet_name
+  address_space           = var.address_space
+
+  tags = {
+    environment = "azure-simple"
+  }
+}
+
+module "subnet" {
+  source = "../../azure/subnet"
+
+  subnet_name          = ["${var.cluster_name}-aks-subnet"]
+  vnet_name            = module.vnet.vnet_name
+  resource_group_name  = data.azurerm_resource_group.resource_group.name
+  address_prefix       = [var.subnet_prefix]
+}
+
+module "aks-gitops" {
+  source = "../../azure/aks-gitops"
+
+  acr_enabled              = var.acr_enabled
+  agent_vm_count           = var.agent_vm_count
+  agent_vm_size            = var.agent_vm_size
+  cluster_name             = var.cluster_name
+  dns_prefix               = var.dns_prefix
+  flux_recreate            = var.flux_recreate
+  gc_enabled               = var.gc_enabled
+  gitops_ssh_url           = var.gitops_ssh_url
+  gitops_ssh_key_path      = var.gitops_ssh_key_path
+  gitops_path              = var.gitops_path
+  gitops_poll_interval     = var.gitops_poll_interval
+  gitops_label             = var.gitops_label
+  gitops_url_branch        = var.gitops_url_branch
+  ssh_public_key           = var.ssh_public_key
+  resource_group_name      = data.azurerm_resource_group.resource_group.name
+  service_principal_id     = var.service_principal_id
+  service_principal_secret = var.service_principal_secret
+  vnet_subnet_id           = tostring(element(module.subnet.subnet_ids, 0))
+  service_cidr             = var.service_cidr
+  dns_ip                   = var.dns_ip
+  docker_cidr              = var.docker_cidr
+  network_plugin           = var.network_plugin
+  network_policy           = var.network_policy
+  oms_agent_enabled        = var.oms_agent_enabled
+  kubernetes_version       = var.kubernetes_version
+}
---
+++ terraform/kubeflow/terraform.tfvars
@@ -0,0 +1,20 @@
+resource_group_name      = "<resource-group-name>" // Must be an existing Resource Group within the subscription
+cluster_name             = "<cluster-name>"
+agent_vm_count           = "3"
+dns_prefix               = "<dns-prefix>"
+service_principal_id     = "<client-id>"
+service_principal_secret = "<client-secret>"
+ssh_public_key           = "ssh-rsa ..." # from node-ssh-key.pub
+gitops_ssh_url           = "git@github.com:<github-user>/<repo>.git" # ssh url to manifest repo
+gitops_ssh_key_path      = "/home/<user>/.ssh/gitops-ssh-key" # path to private gitops repo key
+vnet_name                = "<vnet name>"
+
+#--------------------------------------------------------------
+# Optional variables - Uncomment to use
+#--------------------------------------------------------------
+# gitops_url_branch = "release-123"
+# gitops_poll_interval = "30s"
+# gitops_path = "prod"
+# network_policy = "calico"
+# oms_agent_enabled = "false"
+# gitops_label = "custom-flux-sync"
