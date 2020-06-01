209cafe64e0793fca99882f3f87eacad22b54b9e
jotaylo <jotaylo@microsoft.com>
2020-06-01 16:21:01 -0700

    add postgresql install to mlflow.sh (#40)

--- setup/mlflow/mlflow.sh
+++ setup/mlflow/mlflow.sh
@@ -1,4 +1,10 @@
 # https://hub.kubeapps.com/charts/larribas/mlflow
+
+#install postgresql
+# if upgrading postgresql, set the postgresqlPassword 
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install postgresql bitnami/postgresql --set metrics.enabled=true --set postgresqlDatabase=mlflow
+
 helm repo add larribas https://larribas.me/helm-charts
 helm install mlflow larribas/mlflow --set image.repository="dtzar/mlflow",image.tag="latest",extraArgs.expose-prometheus="yes"
 
