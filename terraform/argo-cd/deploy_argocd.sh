3318dec323e3791922a0bfe09e45f89093af9c02
Michael Tarng <20913254+mtarng@users.noreply.github.com>
2020-06-05 03:48:59 -0700

    changing default argocd namespace to argocd. Need to investigate if that some cluster permissions are bound to that namespace

--- terraform/argo-cd/deploy_argocd.sh
+++ terraform/argo-cd/deploy_argocd.sh
@@ -3,39 +3,56 @@
 
 # https://argoproj.github.io/argo-cd/getting_started/
 
-# Deploy ArgoCD to connected k8s cluster
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+while getopts :n:r:s: option; do
+    case "${option}" in
+    n) ARGO_CD_NAMESPACE=${OPTARG} ;;
+    r) REPO_URL=${OPTARG} ;;
+    s) SSH_PRIVATE_KEY_PATH=${OPTARG} ;;
+    *)
+        echo "Please refer to usage guide on GitHub" >&2
+        exit 1
+        ;;
+    esac
+done
 
-# Set context
-kubectl config set-context --current --namespace argocd
+echo "ARGO_CD_NAMESPACE: $ARGO_CD_NAMESPACE"
+echo "REPO_URL: $REPO_URL"
+echo "SSH_PRIVATE_KEY_PATH: $SSH_PRIVATE_KEY_PATH"
+
+# Deploy ArgoCD to connected k8s cluster
+kubectl create namespace $ARGO_CD_NAMESPACE
+kubectl config set-context --current --namespace $ARGO_CD_NAMESPACE
+kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # Get pod name
-ARGO_SERVER_POD_NAME=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2)
+ARGO_SERVER_POD_NAME=$(kubectl get pods -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2)
 
 # OPTIONAL - Patch svc to LoadBalancer so it is more easily accessed
 # kubectl apply -f argocd-server-loadbalancer.yaml
 
 # Portforward to access UI/connect CLI
 sleep 5
-kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+kubectl port-forward svc/argocd-server 8080:443 &
 PORT_FORWARD_PID=$!
 sleep 3
 # Login - default username is 'admin', password is podname. Can be changed via: argocd account update-password
+echo "Logging into ArgoCD CLI"
 argocd login localhost:8080 --username admin --password $ARGO_SERVER_POD_NAME --insecure
 
-# TODO: add repo credentials 
-# argocd repo or repocreds
-# argocd repo add git@github.com:argoproj/argocd-example-apps.git --ssh-private-key-path ~/.ssh/id_rsa
+# add source repo for gitops
+echo "Adding $REPO_URL to ArgoCD"
+argocd repo add $REPO_URL \
+    --ssh-private-key-path $SSH_PRIVATE_KEY_PATH
 
-# TODO: Add application individually?
+# Won't do this here, but this is how to add an example application through the cli
 # argocd app create guestbook --repo https://github.com/argoproj/argocd-example-apps.git --path guestbook --dest-server https://kubernetes.default.svc --dest-namespace default
 
-## Optional - register external clusters -
+# Optional - register external clusters -
 # https://argoproj.github.io/argo-cd/getting_started/#5-register-a-cluster-to-deploy-apps-to-optional
 
 # Cleanup
 kill $PORT_FORWARD_PID
-scho "Successfully installed ArgoCD on the connected cluster!"
+echo "Successfully installed ArgoCD on the connected cluster!"
 
-
+# Sample command to run this script
+# sh deploy_argocd.sh -n 'argocd' -r 'git@ssh.dev.azure.com:v3/mitarng/cero-demo/fabrikam-manifests' -s '/Users/mtarng/Workspace/kubemlops/terraform/aks-argo-kubeflow-cluster-acr/tempssh/id_rsa'
