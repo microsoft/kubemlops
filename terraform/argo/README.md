aa514cfc9b657ef729f67ba1f82b0ae93d6f2fa1
Michael Tarng <20913254+mtarng@users.noreply.github.com>
2020-06-09 01:50:05 -0700

    Adding WIP argo module notes and variables

--- terraform/argo/README.md
+++ terraform/argo/README.md
@@ -2,6 +2,7 @@
 
 This Work in Progress Terraform module will install [Argo](https://argoproj.github.io/) on a Kubernetes cluster along with a [MinIO](https://min.io/) server.
 
+This module currently has issues with clusterroles. Argo is unable to call any kubernetes apis.
 
 ## TODO:
 - [RBAC](https://argoproj.github.io/docs/argo/workflow-rbac.html) for argo service account.
--- terraform/argo/main.tf
+++ terraform/argo/main.tf
@@ -3,14 +3,14 @@ module "common-provider" {
 }
 
 resource "null_resource" "deploy_argo" {
-  count = var.enable_flux ? 1 : 0
+  count = var.enable_argo ? 1 : 0
 
   provisioner "local-exec" {
     command = "echo 'Need to use this var so terraform waits for kubeconfig ' ${var.kubeconfig_complete};KUBECONFIG=${var.output_directory}/${var.kubeconfig_filename} ${path.module}/deploy_argo.sh"
   }
 
   triggers = {
-    enable_flux   = var.enable_flux
-    flux_recreate = var.flux_recreate
+    enable_argo   = var.enable_argo
+    argo_recreate = var.argo_recreate
   }
 }
