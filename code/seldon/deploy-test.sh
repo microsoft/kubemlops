cbe8d95c7fe4f9cace69153bc9212370da92e994
Eugene Fedorenko <eugene.fedor@gmail.com>
2020-06-12 18:22:30 -0700

    cd

--- code/seldon/deploy-test.sh
+++ code/seldon/deploy-test.sh
@@ -6,5 +6,5 @@ kubectl port-forward svc/mexicanfood-mexicanfood-predictor 8000:8000 -n $NAMESPA
 curl -v -H "Content-Type: application/json" -d '{"image":"https://www.inspiredtaste.net/wp-content/uploads/2018/03/Easy-Ground-Pork-Tacos-Recipe-3-1200.jpg"}' http://localhost:8000/api/v0.1/predictions
 
 CLUSTER_IP=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-curl -v -H "Content-Type: application/json" -d '{"image":"https://www.inspiredtaste.net/wp-content/uploads/2018/03/Easy-Ground-Pork-Tacos-Recipe-3-1200.jpg"}' $CLUSTER_IP/seldon/seldon/mexicanfood/api/v0.1/predictions
+curl -v -H "Content-Type: application/json" -d '{"image":"https://www.inspiredtaste.net/wp-content/uploads/2018/03/Easy-Ground-Pork-Tacos-Recipe-3-1200.jpg"}' $CLUSTER_IP/seldon/$NAMESPACE/mexicanfood/api/v0.1/predictions
 
