e39e1e47c8d167d40885529002bab89a910e6a1b
Sabrina Smai <71526914+sabrina-smai@users.noreply.github.com>
2020-10-29 14:40:57 -0700

    Fixed Github CD Pipeline (#98)
    * Added trigger pipeline
    
    * added header auth
    
    * remove duplicate map of pipeline trigger
    
    * adding access token retrieval
    
    * remove whitespace
    
    * adding on push to invoke cd
    
    * test invoke cd on push
    
    * fix push trigger
    
    * comment handler deploy task
    
    * fixed trial 1
    
    * fixed auth pipline path
    
    * replace token names to match auth file
    
    * added adal import
    
    * test comma
    
    * revert comma addition
    
    * Update cd.yml
    
    * Update cd.yml
    
    * Update cd.yml
    
    * main function
    
    * mlflowrunid
    
    * run id
    
    * auth token
    
    * tokens
    
    * uncomment
    
    * changed pipeline trigger for ML register
    
    * Change pipeline trigger for testing
    
    For testing lint fix
    
    * Fixed long line linting issue
    
    * Comment Handle Deploy event for testing
    
    Comment Handle Deploy event for testing
    
    * added # noqa: E501 override lint
    
    added # noqa: E501 override lint
    
    * adding lint override 
    
    adding lint override
    
    * lint issue fix for -tag
    
    * adding #E261 lint
    
    * fix spacing lint issue
    
    * reverting back docker build
    
    * trigger pipeline test
    
    * replacing runid with test
    
    * echoing run_id
    
    * revert comments handle deploy
    
    Co-authored-by: Eugene Fedorenko <eugene.fedor@gmail.com>

--- code/utils/azure_auth.py
+++ code/utils/azure_auth.py
@@ -1,4 +1,5 @@
 import adal
+import argparse
 
 
 def get_access_token(tenant, clientId, client_secret):
@@ -10,3 +11,37 @@ def get_access_token(tenant, clientId, client_secret):
     context = adal.AuthenticationContext(authority_url)
     token = context.acquire_token_with_client_credentials(GRAPH_RESOURCE, clientId, client_secret)  # noqa: E501
     return token['accessToken']
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--tenant",
+        type=str,
+        required=True,
+        help="Tenant"
+    )
+
+    parser.add_argument(
+        "--service_principal",
+        type=str,
+        required=True,
+        help="Service Principal"
+    )
+
+    parser.add_argument(
+        "--sp_secret",
+        type=str,
+        required=True,
+        help="Service Principal Secret"
+    )
+
+    args = parser.parse_args()
+
+    token = get_access_token(args.tenant, args.service_principal, args.sp_secret)  # noqa: E501, E261
+    return token
+
+
+if __name__ == '__main__':
+    exit(main())
